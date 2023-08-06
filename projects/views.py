30bde2bb0f2ee5c7219d668a6c0c73b7edbda842
Kriukovchankiu <kryukovchanskiu@gmail.com>
2023-08-06 23:03:09 +0300

    Fix bug in projects

--- projects/views.py
+++ projects/views.py
@@ -5,20 +5,32 @@ from accounts.models import User
 
 
 def index_projects(request):
-	user, created = User.objects.get_or_create(username=request.user.username)
-	projects = Project.objects.filter(user = request.user)
-	developers = Project.objects.filter(developers = request.user)
-	titleProjects = "open"
-	context = {
-		'titleProjects': titleProjects,
-		'first_name': user.first_name,
-		'last_name': user.last_name,
-		'position': user.position,
-		'image': user.image,
-		'projects': projects,
-		'developers': developers,
-	}
-	return render(request, 'projects/index.html', context)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user = request.user
+            project.save()
+            form.save_m2m()  # Save the many-to-many relationships (selected developers)
+            return redirect('index_projects')
+    else:
+        form = ProjectForm()
+
+    user, created = User.objects.get_or_create(username=request.user.username)
+    projects = Project.objects.filter(user=request.user)
+    developers = Project.objects.filter(developers=request.user)
+    titleProjects = "open"
+    context = {
+        'titleProjects': titleProjects,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'position': user.position,
+        'image': user.image,
+        'projects': projects,
+        'developers': developers,
+        'form': form,  # Pass the form to the template
+    }
+    return render(request, 'projects/index.html', context)
 
 
 def create_project(request):
